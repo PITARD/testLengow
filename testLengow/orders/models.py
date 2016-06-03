216151a0b21e1c1e4373334e0aefdae861c476ba
Jérémy PITARD <sio.pitard.jeremy@gmail.com>
2016-06-03 16:34:41 +0200

    création du model Order

--- testLengow/orders/models.py
+++ testLengow/orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    order_id = models.CharField(max_length=200, primary_key=True)
+    marketplace = models.CharField(max_length=30)
+    order_purchase_date = models.DateTimeField()
+    order_amout = models.FloatField()
+    order_commission = models.FloatField()
